8c91a64426abbf7ec35cacb028dcb4aa671079cc
Rory Carson <metalhead1504@gmail.com>
2012-04-26 11:35:15 -0500

    Added the (very) basic Card class

---
+++ src/Card.java
@@ -0,0 +1,14 @@
+
+public class Card {
+	private String name;
+	private String type;
+	private String text;
+	private String ability;
+	
+	public Card(){
+		this.name = "";
+		this.type = "";
+		this.text = "";
+		this.ability = "";
+	}
+}
