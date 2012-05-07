27f05e54e45d51576ba6bda505471ebd22ca08a8
Rory Carson <metalhead1504@gmail.com>
2012-05-07 13:13:09 -0500

    Signed-off-by: Rory Carson <metalhead1504@gmail.com>

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+
+public class Person {
+	private String name;
+	private String color;
+	private int[] speed;
+	private int[] strength;
+	private int[] sanity;
+	private int[] knowledge;
+	
+
+	public Person(){
+		this.name = "";
+		this.color = "";
+		this.speed = new int[9];
+		this.strength = new int[9];
+		this.sanity = new int[9];
+		this.knowledge = new int[9];
+		
+	}
+}
